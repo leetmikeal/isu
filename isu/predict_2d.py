d341a694a56f2e5eeb036c024477f29d0cb6d4be
Teppei Tamaki <tamaki@taktpixel.co.jp>
2019-09-25 10:06:29 +0900

    refactoring

--- isu/predict_2d.py
+++ isu/predict_2d.py
@@ -33,28 +33,19 @@ def predict(in_settings, verbose=False):
     config = Config(in_settings)
     model_base = Model2d(config)   
     model = model_base.load()
-    ds = Dataset2d()
-    
-    width, height, imgnum = ds.load_csv(config.csv_path)
-    X_train = ds.image_read(config.input_path+'/*.tif', width, height)
-    mwidth, mheight = X_train.shape[1], X_train.shape[2]
-    X_train_norm = X_train.astype(np.float32)/255.
-    X_train_norm = np.pad(X_train_norm, [(0,0),(0,config.max_size-mwidth),(0,config.max_size-mheight)], 'constant')
-    X_train_norm = X_train_norm.reshape(-1, config.max_size, config.max_size, 1)
-    print('X_train_norm:', X_train_norm.shape)
     
     # model = keras.models.load_model(config.model_2d_path, custom_objects={'loss': unet.soft_dice_loss()})
+    ds = Dataset2d()
+    width, height, imgnum = ds.load_csv(config.csv_path)
+    x_train = ds.image_read(config.input_path+'/*.tif', width, height)
+    x_train_norm, x_width, x_height = reshape(x_train, config.max_size)
 
     if verbose:
+        print('x_train_norm:', x_train_norm.shape)
         print(model.summary())
     
-    Y_pred = model.predict(X_train_norm, batch_size=config.predict_batch_size, verbose=0)
-    Y_pred_bin = np.copy(Y_pred)
-    Y_pred_bin[Y_pred_bin<0.5] = 0
-    Y_pred_bin[Y_pred_bin>=0.5] = 1
-    Y_pred_bin = (Y_pred_bin*255).reshape(-1, config.max_size, config.max_size).astype(np.uint8)
-    Y_pred_bin = Y_pred_bin[:, :mwidth, :mheight]
-    output = np.rollaxis(Y_pred_bin, axis=0, start=3)
+    y_pred = model.predict(x_train_norm, batch_size=config.predict_batch_size, verbose=0)
+    output = postprocess(y_pred, config.max_size, x_width, x_height)
 
     # if not os.path.exists(config.temp2d_path):
     #     if not os.path.exists(config.temp_dir):
@@ -65,6 +56,25 @@ def predict(in_settings, verbose=False):
     # outPath = config.pre_path + '/' + config.dataset + '_temp_'
     os.makedirs(config.temp_dir, exist_ok=True)
     ds.image_save(output, config.temp_dir)
+
+
+def reshape(x_train, size):
+    width, height = x_train.shape[1], x_train.shape[2]
+    x_train_norm = x_train.astype(np.float32)/255.
+    x_train_norm = np.pad(x_train_norm, [(0,0),(0,size-width),(0,size-height)], 'constant')
+    x_train_norm = x_train_norm.reshape(-1, size, size, 1)
+    return x_train_norm, width, height
+
+
+def postprocess(y_pred, size, width, height):
+    y_pred_bin = np.copy(y_pred)
+    y_pred_bin[y_pred_bin<0.5] = 0
+    y_pred_bin[y_pred_bin>=0.5] = 1
+    y_pred_bin = (y_pred_bin*255).reshape(-1, size, size).astype(np.uint8)
+    y_pred_bin = y_pred_bin[:, :width, :height]
+    output = np.rollaxis(y_pred_bin, axis=0, start=3)
+    return output
+
     
 def main(args):
     predict(
