8a49a74c288adf16d10e6eba3b9ce3ab05941ba7
Teppei Tamaki <tamaki@taktpixel.co.jp>
2019-09-27 13:57:57 +0900

    remove training.sh

--- scripts/training.sh
+++
@@ -1,3 +0,0 @@
-set -eu
-
-isu training --in-dir work/ --out-dir work/output --in-weight work/backup_190923-2339/output/trained.h5 --cache-image work/cache/ --epochs 150 --sample-init 300 --sample-val 10 --sample-crop 128 --batch-size 2 --lr-init 0.1 --lr-step 0.1 --lr-epochs 50 --verbose | tee work/output/train.log
